c0c1ea01a3c85646f932ce7b078e7372bed3c9fc
Scorpio <18556134754@163.com>
2023-04-08 19:21:21 +0800

    feat: 初识 ts 类型

---
+++ index.ts
@@ -0,0 +1,49 @@
+let str: string = 'hello ts'
+
+let a: any = 'this is any'
+
+let b: unknown = 'this is unknown'
+
+// unknown 未知类型，相当于类型安全的 any，只允许本身随意赋值，但是不能随意赋值给其它有类型约束的变量
+// any 类型不光本身可以随意赋值，也可以随意赋值给其它类型变量【不推荐】
+str = a
+// str = b error
+
+// 类型断言的两种写法
+str = b as string
+str = <string>b
+
+/**
+ * void 用来表示空，一般表示没有返回值的函数，但是在 Js 中，一般函数正常执行完之后都会默认返回 undefined，即隐式 return undefined
+ * never 表示永远不会返回结果，一般函数执行完都会隐式 return undefined；除了以下这种：函数内部直接 throw 抛出异常
+ */
+function fn(): void {
+  return undefined
+}
+function err(): never {
+  throw new Error('this is error')
+}
+
+// 定义一个函数类型的变量
+let meow: (num: number) => void
+meow = (num) => {
+  console.log('喵'.repeat(num))
+  return 'this is return' // 约束不到位
+}
+
+const eat = (food: string): void => {
+  console.log(`eating ${food}`)
+  // return 'this is return' // 约束到位
+}
+
+let cat: {
+  name: string,
+  age: number,
+  eat: (food: string) => void,
+  [key: string]: unknown // 或者为 any，该对象属性数量未知，可以随便拓展属性
+} = {
+  name: '泡芙',
+  age: 6,
+  eat
+}
+cat.type = '蓝猫'
