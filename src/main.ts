229aa331c2e2411269529ec17b08f7998be922e9
Scorpio <18556134754@163.com>
2023-04-10 20:43:03 +0800

    feat: 集成 webpack

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+const a: string = 'abc'
+
+const b = { name: 'b', age: 12 }
+b.age = 18
+
+console.log(a)
+console.log(b)
---
+++ webpack.config.ts
@@ -0,0 +1,50 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+import * as HtmlWebpackPlugin from 'html-webpack-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+// in case you run into any typescript error when configuring `devServer`
+import 'webpack-dev-server'
+
+const config: webpack.Configuration = {
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+  entry: path.join(__dirname, './src/main.ts'),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: [
+        {
+          loader: 'babel-loader'
+        },
+        {
+          loader: 'ts-loader'
+        }
+      ],
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'TypeScript',
+      template: path.join(__dirname, './index.html'),
+      favicon: path.join(__dirname, './favicon.ico'),
+      inject: true
+    }),
+    new CleanWebpackPlugin()
+  ],
+  devServer: {
+    // open: true,
+    static: {
+      directory: path.join(__dirname, './dist')
+    },
+    compress: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
+
+export default config
