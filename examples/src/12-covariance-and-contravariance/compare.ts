0141990a3529296172c92174ea8decb10b5124db
白夜 <18556134754@163.com>
2023-05-04 22:11:31 +0800

    feat: 协变与逆变的比较 over

--- examples/src/12-covariance-and-contravariance/compare.ts
+++ examples/src/12-covariance-and-contravariance/compare.ts
@@ -20,3 +20,11 @@ const fn2: AnimalFunc = fn;
  * 但是在禁用了 strictFunctionTypes 的情况下，TypeScript 并不会抛出错误，这是因为，在默认情况下，对函数参数类型的检查采用【双变】，
  * 即逆变或协变都被认为可接受
  */
+
+/**
+ * 在 TypeScript ESLint 中，有这么一条规则：method-signature-style，它的意图是约束在接口中声明方法时，
+ * 需要使用 property 而非 method 形式
+ * 进行如此约束的原因，即对于 property 声明，才能在开启严格函数参数类型检查的情况下享受到基于逆变的参数类型检查
+ * 对于 method 声明（以及构造函数声明），其无法享受到这一更严格的检查的原因则是对于如 Array 这样的内置定义，本就希望它的函数方法按照协变方式进行检查【Array 内置方法就是使用 method 声明的】
+ * 因此大部分情况下，确实希望方法参数类型检查是双变的，那就使用 method 声明，否则使用 property 声明【开启 strictFunctionTypes】
+ */
